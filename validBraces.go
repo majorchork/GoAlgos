eb8733eafe5eae6eb9a8d0628ac6297feb2133cb
majorchork <majorchork@gmail.com>
2022-03-22 13:57:34 +0100

    newalgo

--- validBraces.go
+++ validBraces.go
@@ -1,1 +1,47 @@
 package main
+
+import "fmt"
+
+func ValidBraces(braces string) bool {
+	openBraces := []string{}
+
+	for _, eachBrace := range braces {
+		s := string(eachBrace)
+
+		if s == "(" || s == "{" || s == "[" {
+			openBraces = append(openBraces, s)
+		} else {
+			if len(openBraces) == 0 {
+				return false
+			}
+			lastBrace := openBraces[len(openBraces)-1]
+			if s == ")" && lastBrace == "(" {
+				openBraces = openBraces[:len(openBraces)-1]
+				continue
+			} else if s == "}" && lastBrace == "{" {
+				openBraces = openBraces[:len(openBraces)-1]
+				continue
+			} else if s == "]" && lastBrace == "[" {
+				openBraces = openBraces[:len(openBraces)-1]
+				continue
+			}
+			return false
+		}
+	}
+
+	if len(openBraces) == 0 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(ValidBraces("({){}}{{}{{]{))([[][]][)({()}{"))
+	fmt.Println(ValidBraces("{}()[]"))
+	fmt.Println(ValidBraces("[]{}()()()[][][]{}{}[]{}[]())("))
+	fmt.Println(ValidBraces("{(["))
+	fmt.Println(ValidBraces("{()}"))
+	fmt.Println(ValidBraces("({){}}{{))([[][]][)({()}{"))
+	fmt.Println(ValidBraces("[]{}()()()[][][]{}{}[]{}[]()"))
+
+}
