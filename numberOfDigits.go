95a599c39ec84976869ea05b73e96b41ca4ac79b
majorchork <majorchork@gmail.com>
2022-03-27 01:05:46 +0100

    update

--- numberOfDigits.go
+++ numberOfDigits.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func numberOfDigits(n int) int {
+	//Find Number of Digits in Number
+	//Create a function that will return an integer number corresponding to the amount of digits in the given integer num.
+	//	Examples
+	//num_of_digits(1000) ➞ 4
+	//num_of_digits(12) ➞ 2
+	//num_of_digits(1305981031) ➞ 10
+	//num_of_digits(0) ➞ 1
+
 	var product []int
 	if n == 0 {
 		return 1
