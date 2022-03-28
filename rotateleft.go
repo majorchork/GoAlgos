caf242cd6dfd0b64bdd1e369fe35d6dc9adb08c0
majorchork <majorchork@gmail.com>
2022-03-28 15:45:59 +0100

    rotate update

--- rotateleft.go
+++ rotateleft.go
@@ -2,8 +2,25 @@ package main
 
 import "fmt"
 
+/*
+A left rotation operation on an array of size n shifts each of the array's elements 1 unit to the left.
+Given an integer, d, rotate the array that many steps left and return the result.
+
+Function Description
+
+rotateLeft has the following parameters:
+
+int d: the amount to rotate by
+int arr[n]: the array to rotate
+*/
+
 func main() {
 	fmt.Println(rotateLeft(3, []int32{1, 2, 3, 4, 5}))
+	fmt.Println(rotateLeft(4, []int32{1, 2, 3, 4, 5, 6}))
+	fmt.Println(rotateLeft(3, []int32{1, 2, 3, 4, 5}))
+	fmt.Println(rotateLeft(2, []int32{1, 2, 3, 4, 5}))
+	fmt.Println(rotateLeft(1, []int32{1, 2, 3, 4, 5}))
+
 }
 
 func rotateLeft(d int32, arr []int32) []int32 {
