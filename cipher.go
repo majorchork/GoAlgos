da929f22a311d4b119b3c67922e702703a28eecb
majorchork <majorchork@gmail.com>
2022-04-03 23:36:58 +0100

    cipher

--- cipher.go
+++ cipher.go
@@ -3,17 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	//fmt.Println(ceaphar("middle-Outz", 2))
-	//fmt.Println(ceaphar("A friend in need is a friend indeed", 20))
-	//fmt.Println(ceaphar("Always-Look-on-the-Bright-Side-of-Life", 5))
-	//fmt.Println(ceaphar("A Fool and His Money Are Soon Parted.", 27))
-	fmt.Println(ceaphar("One should not worry over things that have already happened and that cannot be changed.", 49))
-	//"Always-Look-on-the-Bright-Side-of-Life"
-	//"A friend in need is a friend indeed"
-	//"U zlcyhx ch hyyx cm u zlcyhx chxyyx"
+	fmt.Println(ceazerCipher("middle-Outz", 2))
+	fmt.Println(ceazerCipher("A friend in need is a friend indeed", 20))
+	fmt.Println(ceazerCipher("Always-Look-on-the-Bright-Side-of-Life", 5))
+	fmt.Println(ceazerCipher("A Fool and His Money Are Soon Parted.", 27))
+	fmt.Println(ceazerCipher("One should not worry over things that have already happened and that cannot be changed.", 49))
 }
 
-func ceaphar(s string, n int) string {
+func ceazerCipher(s string, n int) string {
 	var result string
 	if n > 25 {
 		n = n % 26
